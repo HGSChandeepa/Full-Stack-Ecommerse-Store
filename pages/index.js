7c33ded9442e0c3c46da5d08c7130a13eeecb79d
saminchandeepa <saminchandeepa@gmail.com>
2023-05-25 13:30:46 +0530

    add the sigin with google methode

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-  return <div className=" text-green-500">hello</div>;
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className=" flex items-center justify-center w-screen h-screen bg-gray-800 ">
+        <button
+          onClick={() => signIn("google")}
+          className=" text-white bg-blue-600 px-4 py-2 rounded-md"
+        >
+          Login with Google
+        </button>
+      </div>
+    );
+  } else {
+    return <div>Loged in {session.user.email}</div>;
+  }
 }
